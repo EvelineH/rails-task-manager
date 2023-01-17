91a63ff2e06a381ef21edc813b99b2f201ccc440
Eveline Huiser <evelinehuiser@gmail.com>
2023-01-17 15:24:40 +0100

    done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, except: [:index, :new, :create]
+
   def index
     @tasks = Task.all
   end
@@ -36,6 +38,10 @@ class TasksController < ApplicationController
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details)
